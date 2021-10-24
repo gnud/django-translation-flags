be5c1473bdb03945d869e2b39f07dfe44566e8da
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2021-10-24 14:55:40 +0200

    Fixed paths to match the static directory

--- gulpfile.js
+++ gulpfile.js
@@ -14,22 +14,22 @@ gulp.task('less', function () {
     .pipe(sourcemaps.init())
   .pipe(less())
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./django-translation-flags/static/css'));
+  .pipe(gulp.dest('./django_translation_flags/static/css'));
 });
 
 
 gulp.task('minify-css', function() {
-  return gulp.src(['./django-translation-flags/static/css/django-translation-flags.css'])
+  return gulp.src(['./django_translation_flags/static/css/django-translation-flags.css'])
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./django-translation-flags/static/css'))
+        .pipe(gulp.dest('./django_translation_flags/static/css'))
 });
 
 gulp.task('watch', function () {
   gulp.watch('./assets/less/*.less', ['less']);
-  gulp.watch('./django-translation-flags/static/css/django-translation-flags.css', ['minify-css']);
+  gulp.watch('./django_translation_flags/static/css/django-translation-flags.css', ['minify-css']);
 });
 
 gulp.task('dev', function(){
